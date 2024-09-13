bcf2a1059fb2e068fbe9a8cbf9c203fe0319d880
roberthsu2003 <roberthsu2003@gmail.com>
2024-09-13 15:23:24 +0800

    modify

---
+++ pico/mqtt/public.py
@@ -0,0 +1,40 @@
+from umqtt.simple import MQTTClient
+# Fill in your Adafruit IO Authentication and Feed MQTT Topic details
+mqtt_host = "10.170.1.218"
+mqtt_username = ""  # Your Adafruit IO username
+mqtt_password = ""  # Adafruit IO Key
+mqtt_publish_topic = "test/topic"  # The MQTT topic for your Adafruit IO Feed
+
+# Enter a random ID for this MQTT Client
+# It needs to be globally unique across all of Adafruit IO.
+mqtt_client_id = "somethingreallyrandomandunique123"
+
+# Initialize our MQTTClient and connect to the MQTT server
+mqtt_client = MQTTClient(
+        client_id=mqtt_client_id,
+        server=mqtt_host,
+        user=mqtt_username,
+        password=mqtt_password)
+
+mqtt_client.connect()
+
+# Publish a data point to the Adafruit IO MQTT server every 3 seconds
+# Note: Adafruit IO has rate limits in place, every 3 seconds is frequent
+#  enough to see data in realtime without exceeding the rate limit.
+counter = 0
+try:
+    while True:
+        # Generate some dummy data that changes every loop
+        sine = sin(counter)
+        counter += .8
+        
+        # Publish the data to the topic!
+        print(f'Publish {sine:.2f}')
+        mqtt_client.publish(mqtt_publish_topic, str(sine))
+        
+        # Delay a bit to avoid hitting the rate limit
+        time.sleep(3)
+except Exception as e:
+    print(f'Failed to publish message: {e}')
+finally:
+    mqtt_client.disconnect()
