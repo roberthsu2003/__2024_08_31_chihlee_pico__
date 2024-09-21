ad3b1f709b9e996765c61f19f532467afa984e1b
roberthsu2003 <roberthsu2003@gmail.com>
2024-09-21 14:44:39 +0800

    lesson4_6

---
+++ pico/lesson4/main3.py
@@ -0,0 +1,28 @@
+from machine import Timer, Pin
+
+#tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+#tim.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+green_led = Pin("LED",Pin.OUT)
+green_count = 0
+def green_led_mycallback(t:Timer):
+    global green_count
+    green_count += 1
+    #print(f"目前mycallback被執行:{count}次")
+    green_led.toggle()
+    print("green_led初執行")
+    if green_count >= 10:
+        t.deinit()
+
+green_led_timer = Timer(period=1000,mode=Timer.PERIODIC,callback=green_led_mycallback)
+
+red_count = 0
+def red_led_mycallback(t:Timer):
+    global red_count
+    red_count = red_count + 1
+    #print(f"目前mycallback被執行:{count}次")
+    #green_led.toggle()
+    print("red_led初執行")
+    if red_count >= 10:
+        t.deinit()
+        
+red_led_timer = Timer(period=2000,mode=Timer.PERIODIC,callback=red_led_mycallback)
