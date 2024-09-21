8661f132ce6da8a3a16a96d230dd38ba1271aacb
roberthsu2003 <roberthsu2003@gmail.com>
2024-09-21 14:20:35 +0800

    lesson4_5

--- pico/lesson4/main2.py
+++ pico/lesson4/main2.py
@@ -1,4 +1,13 @@
 from machine import Timer
 
-tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
-tim.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+#tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+#tim.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+count = 0
+def mycallback(t:Timer):
+    global count
+    count = count + 1
+    print(f"目前mycallback被執行:{count}次")
+    if count >= 10:
+        t.deinit()
+
+led_timer = Timer(period=1000,mode=Timer.PERIODIC,callback=mycallback)
