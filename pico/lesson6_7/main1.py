1e695563825e09530101aaa367aa0fd39a95ade1
roberthsu2003 <roberthsu2003@gmail.com>
2024-09-28 11:16:06 +0800

    lesson6_7_04

--- pico/lesson6_7/main1.py
+++ pico/lesson6_7/main1.py
@@ -9,8 +9,12 @@ def do_thing(t):
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
     print(temperature)
+    
+def do_thing1(t):
+    print("do_thing1")
 
 
 
-Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
 
