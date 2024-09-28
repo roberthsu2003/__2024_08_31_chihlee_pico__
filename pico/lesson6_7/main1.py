299d5ec27872cdebfd491b3b21f89c2b9b6e47fa
roberthsu2003 <roberthsu2003@gmail.com>
2024-09-28 11:07:11 +0800

    lesson6_7_03

--- pico/lesson6_7/main1.py
+++ pico/lesson6_7/main1.py
@@ -1,10 +1,16 @@
-import machine
-import time
+from machine import Timer,ADC
 
 
-adc = machine.ADC(4)
-while True:
-    temperature_value = adc.read_u16()
-    print(temperature_value)
-    time.sleep(3)
+
+adc = ADC(4)
+conversion_factor = 3.3 / (65535)
+
+def do_thing(t):
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(temperature)
+
+
+
+Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
 
