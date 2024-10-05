752b4a9414deb53604764787c85d0e6c7d8f81e0
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-05 13:49:44 +0800

    lesson11_05

--- lesson11/main2.py
+++ lesson11/main2.py
@@ -5,8 +5,18 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect('A590301', 'A590301AA')
 
-while not wlan.isconnected() and wlan.status() >= 0:
-    print("Waiting to connect:")
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')
     time.sleep(1)
 
-print(wlan.ifconfig())
+# Handle connection error
+if wlan.status() != 3:    
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0])
