5bb35f1dbd6d3545fb27b7f57ac31b4bf559bc7b
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-05 10:53:14 +0800

    lesson11_02

--- lesson11/main.py
+++ lesson11/main.py
@@ -2,17 +2,18 @@
 from machine import Timer,ADC,Pin,PWM,RTC
 
 adc = ADC(4)
+adc_light = ADC(Pin(28))
 pwm = PWM(Pin(15),freq=50)
 conversion_factor = 3.3 / (65535)
 rtc = RTC()
 
 def do_thing(t):
     reading = adc.read_u16() * conversion_factor
-    temperature = 27 - (reading - 0.706)/0.001721
-    year,month,day,weekly,hours,minuse,seconds,info = rtc.datetime()
-    datetime_str = f"{year}-{month}-{day} {hours}:{minuse}:{seconds}"
-    print(datetime_str)
-    print(temperature)
+    temperature = 27 - (reading - 0.706)/0.001721  
+    print(f'溫度:{temperature}')
+    adc_value = adc_light.read_u16()
+    print(f'光線:{adc_value}')
+    
     
 def do_thing1(t):
     adc1 = ADC(Pin(26))
