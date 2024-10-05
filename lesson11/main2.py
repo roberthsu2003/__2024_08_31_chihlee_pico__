6f16ee715d659afe6bc2d0da53f9614b9dd1598e
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-05 12:00:44 +0800

    lesson11_04

---
+++ lesson11/main2.py
@@ -0,0 +1,12 @@
+import network
+import time
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect('A590301', 'A590301AA')
+
+while not wlan.isconnected() and wlan.status() >= 0:
+    print("Waiting to connect:")
+    time.sleep(1)
+
+print(wlan.ifconfig())
