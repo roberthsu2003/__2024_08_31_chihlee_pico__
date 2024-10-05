5652ab45ce9264bf73d999700138cfd342266830
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-05 11:35:09 +0800

    lesson11_03

--- lesson11/main.py
+++ lesson11/main.py
@@ -1,13 +1,22 @@
+#! usr/bin/micropython
+
+'''
+led->gpio15
+光敏電阻 -> gpio28
+可變電阻 -> gpio26
+內建溫度sensor -> adc最後1pin,共5pin
+'''
 
 from machine import Timer,ADC,Pin,PWM,RTC
 
-adc = ADC(4)
-adc_light = ADC(Pin(28))
-pwm = PWM(Pin(15),freq=50)
-conversion_factor = 3.3 / (65535)
-rtc = RTC()
 
 def do_thing(t):
+    '''
+    :param t:Timer的實體
+    負責偵測溫度和光線
+    每2秒執行1次
+    '''
+    conversion_factor = 3.3 / (65535)
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721  
     print(f'溫度:{temperature}')
@@ -16,13 +25,24 @@ def do_thing(t):
     
     
 def do_thing1(t):
-    adc1 = ADC(Pin(26))
-    duty = adc1.read_u16()
-    pwm.duty_u16(duty)
+    '''
+    :param t:Timer的實體
+    負責可變電阻和改變led的亮度
+    '''    
     
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty)    
     print(f'可變電阻:{round(duty/65535*10)}')
+    
 
+def main():
+    t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+    t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
 
-
-t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
-t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+if __name__ == '__main__':
+    adc = ADC(4) #內建溫度
+    adc1 = ADC(Pin(26)) #可變電阻
+    adc_light = ADC(Pin(28)) #光敏電阻
+    pwm = PWM(Pin(15),freq=50) #pwm led
+    
+    main()
---
+++ lesson11/main1.py
@@ -0,0 +1,14 @@
+def fun1():
+    print("fun1")
+
+def fun2():
+    print("fun2")
+
+def main():
+    print("main function被執行")
+    fun1()
+    fun2()
+
+if __name__ == "__main__":
+    main();
+
