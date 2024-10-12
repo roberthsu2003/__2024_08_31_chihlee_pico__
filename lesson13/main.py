76b3b4173aacec10437be368cd4d29161734d408
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-12 10:17:38 +0800

    lesson13_0

---
+++ lesson13/main.py
@@ -0,0 +1,68 @@
+#! usr/bin/micropython
+
+'''
+led->gpio15
+光敏電阻 -> gpio28
+可變電阻 -> gpio26
+內建溫度sensor -> adc最後1pin,共5pin
+'''
+
+from machine import Timer,ADC,Pin,PWM,RTC
+import binascii
+from umqtt.simple import MQTTClient
+import tools
+
+
+def do_thing(t):
+    '''
+    :param t:Timer的實體
+    負責偵測溫度和光線
+    每2秒執行1次
+    '''
+    conversion_factor = 3.3 / (65535)
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721  
+    print(f'溫度:{temperature}')
+    adc_value = adc_light.read_u16()
+    print(f'光線:{adc_value}')
+    
+    
+def do_thing1(t):
+    '''
+    :param t:Timer的實體
+    負責可變電阻和改變led的亮度
+    '''    
+    
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty)
+    light_level = round(duty/65535*10)
+    print(f'可變電阻:{light_level}')
+    mqtt.publish('SA-01/LIGHT_LEVEL', f'{light_level}')
+    
+
+def main():
+    pass
+        
+
+if __name__ == '__main__':
+    adc = ADC(4) #內建溫度
+    adc1 = ADC(Pin(26)) #可變電阻
+    adc_light = ADC(Pin(28)) #光敏電阻
+    pwm = PWM(Pin(15),freq=50) #pwm led
+    #連線internet
+    try:
+        tools.connect()
+    except RuntimeError as e:
+        print(e)
+    except Exception:
+        print('不知明的錯誤')
+    else:
+        #MQTT
+        SERVER = "192.168.0.252"
+        CLIENT_ID = binascii.hexlify(machine.unique_id())
+        mqtt = MQTTClient(CLIENT_ID, SERVER,user='pi',password='raspberry')
+        mqtt.connect()
+        t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+        t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)   
+    
+    main()
