d60dbb064811965f3c04a2da997140f6ccdfc6c4
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-12 15:48:42 +0800

    modify

--- lesson13/computer/index.py
+++ lesson13/computer/index.py
@@ -1,11 +1,22 @@
 import paho.mqtt.client as mqtt
 
+def record():
+    pass
+
 def on_connect(client, userdata, flags, reason_code, properties):
     #連線bloker成功時,只會執行一次
     client.subscribe("SA-01/#")
 
 def on_message(client, userdata, msg):
-    print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+    global led_origin_value
+    topic = msg.topic
+    value = msg.payload.decode()
+    if topic == 'SA-01/LED_LEVEL':
+        led_value = int(value)
+        if led_value != led_origin_value:
+            led_origin_value = led_value
+            print(f'led_value:{led_value}')
+    #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
 
 def main():
     client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
@@ -20,4 +31,5 @@ def main():
 
 
 if __name__ == "__main__":
+    led_origin_value = 0 
     main()
